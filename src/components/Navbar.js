292099128c921342a4dd9be8cda9b066e03f9bda
gabyballester <gaby.ballester@gmail.com>
2020-10-18 15:49:21 +0200

    Instalado react-router-dom y creados componentes base

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+// rfce -> generación automática de componente
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar"></div>
+        </>
+    )
+}
+
+export default Navbar
